632e54047917f6d831aa2b527a8b9c8cf185a628
mk <midaslmg94@gmail.com>
2020-05-04 01:47:24 +0900

    등록 Service - 트랜잭션, 도메인 기능 간의 순서를 보장

---
+++ src/main/java/com/springboot/restfulboard/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.springboot.restfulboard.service;
+
+import com.springboot.restfulboard.domain.posts.PostsRepository;
+import com.springboot.restfulboard.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
